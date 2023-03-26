549f63b61f14fe7346361231d99b2f13602b6c1c
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 17:02:00 +0200

    Beírsz egy számot, és addig a számig kiírja

---
+++ 029. eddigird/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _029.eddigird
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot! ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            int eddigird = 1;
+            
+
+            while (eddigird < szam)
+            //for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine(eddigird);
+                eddigird++;
+            }
+    
+            Console.ReadKey();
+
+        }
+        
+        
+    }
+}
